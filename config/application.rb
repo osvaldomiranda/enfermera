ae65e9fc18c49f5ef16375832d85291ebb5c5322
Osvaldo Miranda S <osvaldo.omiranda@gmail.com>
2015-10-21 23:31:30 -0300

    Assets precompile

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,11 @@ module Hikerdata
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.precompile << Proc.new { |path|
+      if path =~ /\.(eot|svg|ttf|woff)\z/
+        true
+      end
+    }
+
   end
 end
