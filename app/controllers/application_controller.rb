06f54f8d47da67937610e3d021d01ef597babee3
osvaldomiranda <osvaldo.omiranda@gmail.com>
2016-04-20 23:49:59 -0300

    [ROUTES]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-<<<<<<< HEAD
-  #before_filter :authenticate_user!
-=======
- 
->>>>>>> e30b8ac6901e0f090b17c0b2df0aa91e1c0041f1
+
 
   def respond_modal_with(*args, &blk)
     options = args.extract_options!
