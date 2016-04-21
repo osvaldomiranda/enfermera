0593d985c92833b3e547d177c75556d3f7646464
osvaldomiranda <osvaldo.omiranda@gmail.com>
2016-04-21 00:21:34 -0300

    [FIX] Ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,24 +8,6 @@ class Ability
       can :login, User
     elsif user.role? :observer
       can :read, :all
-    else 
-      can :manage, :inscription
-    # elsif user.role? :realtor
-    #   can :reservation, Reserva
-    # elsif user.role? :logistics
-    #   can :manage, Reserva
-    #   can [:create, :read, :update, :upload, :edit ], Proyecto
-    #   can :manage, :logistica
-    # elsif user.role? :logistics_admin  
-    #   can :destroy, Proyecto
-    # elsif user.role? :promise
-    #   can :manage, promise
-    # elsif user.role? :invoice
-    #   can :manage, Invoice
-    # elsif user.role? :invoice_admin  
-    #   can :manage, Invoice 
-    else
-      can :manage, :inscription     
     end
   end
 end
