d6c6620b06b108d0040aceac439ebe87d52809a2
osvaldomiranda <osvaldo.omiranda@gmail.com>
2016-09-25 13:00:39 -0400

    [ADD] config wikedpdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -2,5 +2,8 @@ WickedPdf.config = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
   #:exe_path => '/usr/local/bin/wkhtmltopdf.sh'
-  :exe_path => '/Users/Osvaldo/.rbenv/shims/wkhtmltopdf'
+  # :exe_path => '/Users/Osvaldo/.rbenv/shims/wkhtmltopdf'
+  :page_size=>"Letter",
+  :exe_path => '/usr/local/bin/wkhtmltopdf',
+  :encoding => "UTF-8"
 }
