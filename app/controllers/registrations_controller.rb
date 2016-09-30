863edaef3ff527c7a79f80ad5df95dc133ae5b1f
osvaldomiranda <osvaldo.omiranda@gmail.com>
2016-09-30 10:19:35 -0400

    [UPDATE] corrige registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,6 +10,7 @@ class RegistrationsController < Devise::RegistrationsController
     user.email = params[:user][:email]
     user.password = params[:user][:password]
     user.password_confirmation = params[:user][:password_confirmation]
+    user.rut = params[:user][:rut]
     user.save
 
     person = Person.new
