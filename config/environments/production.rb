c3f797f631ec1dab45dc8d3d1e4276b3c1ec4421
osvaldomiranda <osvaldo.omiranda@gmail.com>
2016-10-12 00:31:46 -0400

    [ADD] ASSETS precompile = false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Enfermera::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
+  config.assets.precompile =  ['*.js', '*.css'] 
   # Generate digests for assets URLs.
   config.assets.digest = true
 
