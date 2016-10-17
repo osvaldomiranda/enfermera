3887aaf0cfa9ed9de71e386717467157b8f638d0
osvaldomiranda <osvaldo.omiranda@gmail.com>
2016-10-16 21:27:02 -0400

    [ADD] server_assets_static=true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@ Enfermera::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+
+  config.assets.paths << Rails.root.join("assets", "assets/images")
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
 
