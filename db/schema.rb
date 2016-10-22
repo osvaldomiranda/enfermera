2380ff77e27856b11f72666e5aded1dad6d414e3
osvaldomiranda <osvaldo.omiranda@gmail.com>
2016-10-22 12:36:49 -0400

    [ADD] Froala editor

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019141140) do
+ActiveRecord::Schema.define(version: 20161022154937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,20 +29,6 @@ ActiveRecord::Schema.define(version: 20161019141140) do
     t.string   "imagen"
     t.string   "documento"
     t.string   "estado"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "bootsy_image_galleries", force: true do |t|
-    t.integer  "bootsy_resource_id"
-    t.string   "bootsy_resource_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "bootsy_images", force: true do |t|
-    t.string   "image_file"
-    t.integer  "image_gallery_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
