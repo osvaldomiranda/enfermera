f22e412f6bac4f7c92deab93d9ab2eff14c4330a
osvaldomiranda <osvaldo.omiranda@gmail.com>
2018-08-09 21:54:49 -0400

    [ADD] uploaders

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,8 +8,8 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
 
-  #storage :file
-  storage :fog
+  storage :file
+  #storage :fog
 
 
   def store_dir
